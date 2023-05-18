c1b16c62151d20cfb0d185ccba9df782204ca214
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-05-18 14:13:27 +0800

    Vue跟组件

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+import {defineComponent} from "vue";
+// 顶部
+import top from './components/top/index.vue'
+// 内容区
+import blog from './components/blog/index.vue'
+// 底部
+import bottom from './components/bottom/index.vue'
+// 留言板
+import messageBoard from './components/messageBoard/index.vue'
+
+export default defineComponent({
+  components: {
+    top,
+    blog,
+    bottom,
+    messageBoard,
+  },
+})
+</script>
+<template>
+  <!--    <top></top>-->
+  <blog></blog>
+  <message-board></message-board>
+  <bottom></bottom>
+</template>
+<style>
+</style>
