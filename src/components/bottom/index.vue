b38b6a86ade938677d4d3c4e0ab072aa4584f36a
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-05-18 18:13:03 +0800

    底部模块,声明版权信息开发团队等

---
+++ src/components/bottom/index.vue
@@ -0,0 +1,38 @@
+<!--author:steam-404-->
+<!--底部信息-->
+<script setup lang="ts">
+import {ref} from 'vue'
+// 版权
+// 开始年份
+const startYear=ref(2023)
+// 当前年份
+const data=new Date();
+const nowYear=ref(data.getFullYear())
+</script>
+
+<template>
+  <h5>
+    © {{startYear}}-{{nowYear}}
+    MIT License
+    Designed and Developed with ❤️ by
+    [<a href="https://github.com/Blog-Information">建建</a>]
+  </h5>
+</template>
+
+<style scoped>
+*{
+  margin: 0;
+  padding: 0;
+}
+h5{
+  background-color: #1F1E37;
+  margin-top: 20vh;
+  padding: 20vh;
+  font-family: Helvetica,serif;
+  text-align: center;
+  font-size: 3vh;
+}
+a{
+  color:  #409EFF;
+}
+</style>
