8c4aaaaff969e757cb0c1a4bf33a8b7c2f306538
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-05-18 18:14:12 +0800

    顶部,导航,菜单等

---
+++ src/components/top/index.vue
@@ -0,0 +1,34 @@
+<!--author:steam-404-->
+<!--顶部导航栏-->
+<script setup lang="ts">
+</script>
+
+<template>
+  <div id="top">
+    <div id="top1">steam-404</div>
+    <div id="top2">
+      <a href="https://github.com/steam-404">
+        Go to my GitHub
+      </a>
+      👉
+    </div>
+  </div>
+</template>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+#top {
+  background-color: #409EFF;
+  height: 5vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+#top2 > a{
+  color: moccasin;
+}
+</style>
