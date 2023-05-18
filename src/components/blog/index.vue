25b76a8e928d081ba104918962250ecbd8e6ba1c
steam-404 <71576964+steam-404@users.noreply.github.com>
2023-05-18 18:12:18 +0800

    内容区模块,内容暂定

---
+++ src/components/blog/index.vue
@@ -0,0 +1,27 @@
+<!--author:steam-404-->
+<!--博客介绍-->
+<template>
+  <div id="headImage">
+    <!--    头像-->
+    <img src="../../../public/ikun.png" alt="loading...">
+    <h4>Hi there 👋,I'm jian</h4>
+    <h3>I am from Shaanxi, China</h3>
+  </div>
+<!--  <img-->
+<!--      src="https://camo.githubusercontent.com/762c9c86565369b3cbcac776397f47e12674ab2b6ce8f6ebb768c4cbf666832e/68747470733a2f2f77616b6174696d652e636f6d2f62616467652f757365722f37383535306463612d346137342d346438332d613362642d3238623936346264346132632e737667"-->
+<!--      alt=""-->
+<!--  >-->
+</template>
+<script>
+
+</script>
+<style>
+#headImage {
+  text-align: center;
+}
+
+#headImage img {
+  border-radius: 50%;
+  width: 30vh;
+}
+</style>
